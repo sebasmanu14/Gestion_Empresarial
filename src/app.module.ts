83bf560cdb6d4a3e0d426b942fe8c4e04f703e1d
sebasmanu14 <sas.manobanda@yavirac.edu.ec>
2022-06-29 12:02:11 -0500

    reslvido el app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,8 @@ import { ProductsController } from './controllers/products.controller';
 import { ProductsService } from './services/products/products.service';
 import { SchoolPeriodService } from './services/school-period/school-period.service';
 import { SchoolPeriodController } from './controllers/school-period/school-period.controller';
-<<<<<<< HEAD
 import { BusinessTutorsService } from './services/business-tutors/business-tutors.service';
 import { BusinessTutorsController } from './controllers/business-tutor/business-tutors.controller';
-
-=======
 import { DualTrainingService } from './services/dual-training/dual-training.service';
 import { DualTrainingController } from './controllers/dual-training/dual-training.controller';
 import { StudentService } from './services/student/student.service';
@@ -22,12 +19,8 @@ import { LevelController } from './controllers/level/level.controller';
 import { LevelService } from './services/level/level.service';
 import { InstitutionalTutorController } from './controllers/institutional-tutor/institutional_tutor.controller';
 import { InstitutionalTutorService } from './services/institutional-tutor/institutional-tutor.service';
-<<<<<<< HEAD
 import { CareerService } from './services/career/career.service';
 import { CareerController } from './controllers/career/career.controller';
-=======
->>>>>>> bb6a9865013a72cae3859e4dc614675ff4091d10
->>>>>>> d408c48ccaceca16caa77e85a0462d861ebd15a1
 @Module({
   imports: [],
   controllers: [
@@ -35,29 +28,25 @@ import { CareerController } from './controllers/career/career.controller';
     CategoriesController,
     ProductsController,
     SchoolPeriodController,
-<<<<<<< HEAD
     BusinessTutorsController,
-  ],
-  providers: [AppService, ProductsService, SchoolPeriodService, BusinessTutorsService],
-=======
     DualTrainingController,
     StudentController,
     CompanyController,
-    LevelController,
     InstitutionalTutorController,
-    CareerController,
+    LevelController,
+    CareerController
   ],
   providers: [
     AppService,
     ProductsService,
     SchoolPeriodService,
+    BusinessTutorsService,
     DualTrainingService,
     StudentService,
-    CompanyService,
     LevelService,
+    CompanyService,
     InstitutionalTutorService,
-    CareerService,
+    CareerService
   ],
->>>>>>> bb6a9865013a72cae3859e4dc614675ff4091d10
 })
 export class AppModule {}
